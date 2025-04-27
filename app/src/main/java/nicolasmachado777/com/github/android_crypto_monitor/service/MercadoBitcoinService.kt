c46ddae30a6176b0692a5342e6b9c2d48a0a095a
nicolasmachado777 <nicolasforworks@gmail.com>
2025-04-27 15:33:38 -0300

    subindo CP2

---
+++ app/src/main/java/nicolasmachado777/com/github/android_crypto_monitor/service/MercadoBitcoinService.kt
@@ -0,0 +1,11 @@
+package nicolasmachado777.com.github.android_crypto_monitor.service;
+
+import nicolasmachado777.com.github.android_crypto_monitor.model.TickerResponse;
+import retrofit2.Response;
+import retrofit2.http.GET;
+
+interface MercadoBitcoinService {
+
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker(): Response<TickerResponse>
+}
